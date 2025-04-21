f3d13ad8e5df2ce1fc078d78ac57715fa9d84b2d
emmawidl <48824463+emmawidl@users.noreply.github.com>
2025-04-21 20:20:27 +0200

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,19 @@
+import pandas as pd
+from xgboost import XGBClassifier
 
+class MatchPredictor:
+    def __init__(self):
+        self.model = XGBClassifier(use_label_encoder=False, eval_metric='logloss')
+
+    def train(self, df):
+        X = df.drop('winner', axis=1)
+        y = df['winner']
+        self.model.fit(X, y)
+
+    def predict(self, p1, p2):
+        features = [
+            p1['elo'] - p2['elo'],
+            p1['ranking'] - p2['ranking'],
+            p1['clay_win_pct'] - p2['clay_win_pct']
+        ]
+        return 1 if self.model.predict([features])[0] == 1 else 2
