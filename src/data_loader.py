3222ad41d13f0362adc4375ca3036612e5b45b49
emmawidl <48824463+emmawidl@users.noreply.github.com>
2025-04-21 20:20:44 +0200

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,1 +1,12 @@
-
+# In future, this will fetch real ATP data
+def get_mock_players():
+    return [
+        {'name': 'Carlos Alcaraz', 'ranking': 3, 'elo': 2200, 'clay_win_pct': 85},
+        {'name': 'Novak Djokovic', 'ranking': 1, 'elo': 2300, 'clay_win_pct': 80},
+        {'name': 'Daniil Medvedev', 'ranking': 5, 'elo': 2100, 'clay_win_pct': 60},
+        {'name': 'Jannik Sinner', 'ranking': 2, 'elo': 2250, 'clay_win_pct': 78},
+        {'name': 'Stefanos Tsitsipas', 'ranking': 7, 'elo': 2150, 'clay_win_pct': 75},
+        {'name': 'Casper Ruud', 'ranking': 6, 'elo': 2120, 'clay_win_pct': 82},
+        {'name': 'Andrey Rublev', 'ranking': 8, 'elo': 2100, 'clay_win_pct': 70},
+        {'name': 'Alexander Zverev', 'ranking': 4, 'elo': 2180, 'clay_win_pct': 76},
+    ]
